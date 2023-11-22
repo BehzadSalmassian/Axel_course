7aa66359212dde3a8ff3eb7a8b6ddacaec22db86
Behzad <Behzadsalmasianbtc@gmail.com>
2023-11-22 17:36:20 +0100

    fill lists

--- first.py
+++ first.py
@@ -3,3 +3,17 @@
 if __name__ == "__main__":
     print("Simple script to plot x and y values.")
 
+    # Step 2: Fill Lists
+    import math
+
+    xval = []
+    yval = []
+
+    for x in range(-50, 51):
+        xval.append(x * 0.1)
+
+    for x in xval:
+        yval.append(math.sin(x))
+
+    print("xval:", xval)
+    print("yval:", yval)
